cd9e359ecabc9f4ef69734d24bcfe44180da7e06
Nikitha22735 <nikitha.thripuram@gmail.com>
2026-05-13 18:52:55 +0530

    up

--- conftest.py
+++ conftest.py
@@ -31,12 +31,9 @@ def shoppingCartObj(page: Page):
 @pytest.fixture(scope="function")
 def launchingAmazon(page: Page):
     page.goto("https://www.amazon.in/")
-    continue_btn = page.get_by_role(
-        "button",
-        name="Continue shopping"
-    )
+    continue_btn = page.locator("//*[contains(text(),'ontinue s')]")
 
-    if continue_btn.is_visible(timeout=5000):
+    if continue_btn.is_visible(timeout=15000):
         continue_btn.click()
 
 
--- tests/test_variabelsFromGit.py
+++ tests/test_variabelsFromGit.py
@@ -6,7 +6,7 @@ def test_Git():
     base_url = os.getenv("BASE_URL")
     username = os.getenv("USERNAME")
     password = os.getenv("PASSWORD")
-    token = os.getenv("API_TOKEN")
 
     print(base_url)
     print(username)
+    print(password)
